abe98a63c1957f33bccea0bc710f298da547c95b
twopheat <twopheat@gmail.com>
2020-04-07 23:52:58 -0700

    Pushing again

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,7 @@
 var mongoose = require("mongoose");
-var db = require("../models/transaction.js");
+var db = require("../models/book.js");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true
 });
 
-
-var budgetSeed = [];
